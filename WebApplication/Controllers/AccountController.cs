88082472592bc44386f11cd962da65bce952fa23
ImagineDragon <roma.09089886@gmail.com>
2019-09-28 11:55:52 +0300

    almost done

--- WebApplication/Controllers/AccountController.cs
+++ WebApplication/Controllers/AccountController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 using WebApplication.Models;
 
 namespace WebApplication.Controllers
@@ -88,8 +90,13 @@ namespace WebApplication.Controllers
             return View(files);
         }
 
-        public ActionResult FileUpload()
+        public ActionResult UploadFile()
         {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login");
+            }
+
             if (Request.Files.Count > 0)
             {
                 UserContext db = new UserContext();
@@ -101,16 +108,103 @@ namespace WebApplication.Controllers
                     if (file != null && file.ContentLength > 0)
                     {
                         var fileName = Path.GetFileName(file.FileName);
-                        var path = Path.Combine(Server.MapPath("~/UsersFiles/"), fileName);
-                        file.SaveAs(path);
+                        var path = Server.MapPath("~/UsersFiles/" + User.Identity.Name + "/");
+                        if (!Directory.Exists(path))
+                        {
+                            Directory.CreateDirectory(path);
+                        }
+                        var filePath = Path.Combine(path, fileName);
+                        if (!System.IO.File.Exists(filePath))
+                        {
+                            file.SaveAs(filePath);
 
-                        int userId = db.Users.FirstOrDefault(u => u.UserName == User.Identity.Name).Id;
+                            int userId = db.Users.FirstOrDefault(u => u.UserName == User.Identity.Name).Id;
 
-                        db.Files.Add(new Models.File { FileName = fileName, FileSize = file.ContentLength, UploadingDate = DateTime.Now, UserId = userId });
+                            db.Files.Add(new Models.File { FileName = fileName, FileSize = file.ContentLength, UploadingDate = DateTime.Now, UserId = userId });
+                        }
                     }
                 }
                 db.SaveChanges();
             }
+            return RedirectToAction("Files");
+        }
+
+        public ActionResult DeleteFile(int id)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login");
+            }
+
+            var db = new UserContext();
+
+            var file = db.Files.FirstOrDefault(f => f.FileId == id);
+
+            var path = Server.MapPath("~/UsersFiles/" + User.Identity.Name + "/" + file.FileName);
+
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+
+            db.Files.Remove(file);
+            db.SaveChanges();
+
+            return RedirectToAction("Files");
+        }
+
+        public ActionResult DownloadFile(string fileName)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login");
+            }
+
+            var path = Server.MapPath("~/UsersFiles/" + User.Identity.Name + "/" + fileName);
+
+            if (System.IO.File.Exists(path))
+            {
+                byte[] fileBytes = System.IO.File.ReadAllBytes(path);
+
+                return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+            }
+
+            return RedirectToAction("Files");
+        }
+
+        public ActionResult ExportToExcel()
+        {
+            var db = new UserContext();
+
+            var userId = db.Users.FirstOrDefault(u => u.UserName == User.Identity.Name).Id;
+
+            var files = db.Files.Where(f => f.UserId == userId).Select(a => new { a.FileName, a.FileSize, a.UploadingDate }).ToList();
+
+            GridView gridview = new GridView();
+            gridview.DataSource = files;
+            gridview.DataBind();
+
+            // Clear all the content from the current response
+            Response.ClearContent();
+            Response.Buffer = true;
+            // set the header
+            Response.AddHeader("content-disposition", "attachment; filename = files.xls");
+            Response.ContentType = "application/ms-excel";
+            Response.Charset = "";
+            // create HtmlTextWriter object with StringWriter
+            using (StringWriter sw = new StringWriter())
+            {
+                using (HtmlTextWriter htw = new HtmlTextWriter(sw))
+                {
+                    // render the GridView to the HtmlTextWriter
+                    gridview.RenderControl(htw);
+                    // Output the GridView content saved into StringWriter
+                    Response.Output.Write(sw.ToString());
+                    Response.Flush();
+                    Response.End();
+                }
+            }
+
             return RedirectToAction("Files");
         }
 
